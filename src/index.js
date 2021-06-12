645ecae6c04d13e8e85c9abedeac207a8ebf7ff3
yuna535 <58472579+yuna535@users.noreply.github.com>
2021-06-12 17:58:36 +0900

    テキストボックスに入力した値を未完了のtodo-areaに表示

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //　div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //　未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
+};
+
+document
+  .getElementById("add-buttom")
+  .addEventListener("click", () => onClickAdd());
